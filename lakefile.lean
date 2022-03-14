870f778dfeec54e571aa6d5a40ea4bef7efe1ce9
Xubai Wang <self@xubai.wang>
2022-03-14 16:54:20 +0800

    refactor: use include_str from mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -3,4 +3,10 @@ open Lake DSL
 
 package Unicode {
   defaultFacet := PackageFacet.staticLib
+  dependencies := #[
+    {
+      name := `mathlib
+      src := Source.git "https://github.com/leanprover-community/mathlib4" "0bdee9fa7f180c43a9ca75fd714e49125d0a2861"
+    }
+  ]
 }
