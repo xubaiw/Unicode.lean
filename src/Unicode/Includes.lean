7263544c29c92e78aa77cf19f4070d7f5514bfd8
Anders Christiansen Sørby <anders@sorby.xyz>
2022-04-24 23:34:11 +0200

    feat: Add nix flake build

--- src/Unicode/Includes.lean
+++ src/Unicode/Includes.lean
@@ -1,8 +1,9 @@
-import Mathlib.Util.IncludeStr
+-- import Mathlib.Util.IncludeStr
 import Std.Data.HashMap
+import Lean
 
-open Std
-
+open Std System
+ 
 partial def String.splitOn'Aux (s sep : String) (b : Pos) (i : Pos) (j : Pos) (r : Array String) : Array String :=
   if s.atEnd i then
     if sep.atEnd j then
@@ -22,6 +23,24 @@ partial def String.splitOn'Aux (s sep : String) (b : Pos) (i : Pos) (j : Pos) (r
 def String.splitOn' (s : String) (sep : String := " ") : Array String :=
   if sep == "" then #[s] else splitOn'Aux s sep 0 0 0 #[]
 
+def Option.flatten : (o : Option (Option A)) → Option A
+  | some opt => opt
+  | none => none
+
+def System.FilePath.parentN (fp : FilePath) : Nat → Option FilePath
+  | 0 => fp.parent
+  | Nat.succ n => (fp.parentN n).map FilePath.parent |> Option.flatten
+
+namespace Mathlib.Util
+
+elab (name := includeStr) "include_ucd_str " str:str : term => do
+  let some str := str.isStrLit? | Lean.Elab.throwUnsupportedSyntax
+  let srcDir := System.FilePath.mk ((← IO.getEnv "UCD_DIR").getD "./UCD")
+  let path := srcDir / str
+  Lean.mkStrLit <$> IO.FS.readFile path
+
+end Mathlib.Util
+
 namespace Unicode
 
 /-!
@@ -31,167 +50,167 @@ namespace Unicode
 -/
 
 /-- Includes the ArabicShaping.txt string. -/
-def arabicShapingStr := include_str "../UCD/ArabicShaping.txt"
+def arabicShapingStr := include_ucd_str "ArabicShaping.txt"
 
 /-- Includes the BidiBrackets.txt string. -/
-def bidiBracketsStr := include_str "../UCD/BidiBrackets.txt"
+def bidiBracketsStr := include_ucd_str "BidiBrackets.txt"
 
 /-- Includes the BidiMirroring.txt string. -/
-def bidiMirroringStr := include_str "../UCD/BidiMirroring.txt"
+def bidiMirroringStr := include_ucd_str "BidiMirroring.txt"
 
 /-- Includes the NormalizationCorrections.txt string. -/
-def normalizationCorrectionsStr := include_str "../UCD/NormalizationCorrections.txt"
+def normalizationCorrectionsStr := include_ucd_str "NormalizationCorrections.txt"
 
 /-- Includes the Blocks.txt string. -/
-def blocksStr := include_str "../UCD/Blocks.txt"
+def blocksStr := include_ucd_str "Blocks.txt"
 
 /-- Includes the NushuSources.txt string. -/
-def nushuSourcesStr := include_str "../UCD/NushuSources.txt"
+def nushuSourcesStr := include_ucd_str "NushuSources.txt"
 
 /-- Includes the PropList.txt string. -/
-def propListStr := include_str "../UCD/PropList.txt"
+def propListStr := include_ucd_str "PropList.txt"
 
 /-- Includes the CaseFolding.txt string. -/
-def caseFoldingStr := include_str "../UCD/CaseFolding.txt"
+def caseFoldingStr := include_ucd_str "CaseFolding.txt"
 
 /-- Includes the CompositionExclusions.txt string. -/
-def compositionExclusionsStr := include_str "../UCD/CompositionExclusions.txt"
+def compositionExclusionsStr := include_ucd_str "CompositionExclusions.txt"
   
 /-- Includes the DerivedAge.txt string. -/
-def derivedAgeStr := include_str "../UCD/DerivedAge.txt"
+def derivedAgeStr := include_ucd_str "DerivedAge.txt"
 
 /-- Includes the ScriptExtensions.txt string. -/
-def scriptExtensionsStr := include_str "../UCD/ScriptExtensions.txt"
+def scriptExtensionsStr := include_ucd_str "ScriptExtensions.txt"
 
 /-- Includes the DerivedCoreProperties.txt string. -/
-def derivedCorePropertiesStr := include_str "../UCD/DerivedCoreProperties.txt"
+def derivedCorePropertiesStr := include_ucd_str "DerivedCoreProperties.txt"
 
 /-- Includes the Scripts.txt string. -/
-def scriptsStr := include_str "../UCD/Scripts.txt"
+def scriptsStr := include_ucd_str "Scripts.txt"
 
 /-- Includes the DerivedNormalizationProps.txt string. -/
-def derivedNormalizationPropsStr := include_str "../UCD/DerivedNormalizationProps.txt"
+def derivedNormalizationPropsStr := include_ucd_str "DerivedNormalizationProps.txt"
 
 /-- Includes the SpecialCasing.txt string. -/
-def specialCasingStr := include_str "../UCD/SpecialCasing.txt"
+def specialCasingStr := include_ucd_str "SpecialCasing.txt"
 
 /-- Includes the EastAsianWidth.txt string. -/
-def eastAsianWidthStr := include_str "../UCD/EastAsianWidth.txt"
+def eastAsianWidthStr := include_ucd_str "EastAsianWidth.txt"
 
 /-- Includes the EquivalentUnifiedIdeograph.txt string. -/
-def equivalentUnifiedIdeographStr := include_str "../UCD/EquivalentUnifiedIdeograph.txt"
+def equivalentUnifiedIdeographStr := include_ucd_str "EquivalentUnifiedIdeograph.txt"
 
 /-- Includes the HangulSyllableType.txt string. -/
-def hangulSyllableTypeStr := include_str "../UCD/HangulSyllableType.txt"
+def hangulSyllableTypeStr := include_ucd_str "HangulSyllableType.txt"
 
 /-- Includes the UnicodeData.txt string. See `unicodeDataMap`. -/
-def unicodeDataStr := include_str "../UCD/UnicodeData.txt"
+def unicodeDataStr := include_ucd_str "UnicodeData.txt"
 
 /-- Includes the IndicPositionalCategory.txt string. -/
-def indicPositionalCategoryStr := include_str "../UCD/IndicPositionalCategory.txt"
+def indicPositionalCategoryStr := include_ucd_str "IndicPositionalCategory.txt"
 
 /-- Includes the VerticalOrientation.txt string. -/
-def verticalOrientationStr := include_str "../UCD/VerticalOrientation.txt"
+def verticalOrientationStr := include_ucd_str "VerticalOrientation.txt"
 
 /-- Includes the IndicSyllabicCategory.txt string. -/
-def indicSyllabicCategoryStr := include_str "../UCD/IndicSyllabicCategory.txt"
+def indicSyllabicCategoryStr := include_ucd_str "IndicSyllabicCategory.txt"
 
 /-- Includes the Jamo.txt string. -/
-def jamoStr := include_str "../UCD/Jamo.txt"
+def jamoStr := include_ucd_str "Jamo.txt"
 
 /-- Includes the LineBreak.txt string. -/
-def lineBreakStr := include_str "../UCD/LineBreak.txt"
+def lineBreakStr := include_ucd_str "LineBreak.txt"
 
 /-- Includes the NameAliases.txt string. -/
-def nameAliasesStr := include_str "../UCD/NameAliases.txt"
+def nameAliasesStr := include_ucd_str "NameAliases.txt"
 
 /-!
   ### Auxiliary Subdirectory
 -/
 
 /-- Includes the GraphemeBreakProperty.txt string. -/
-def graphemeBreakPropertyStr := include_str "../UCD/auxiliary/GraphemeBreakProperty.txt"
+def graphemeBreakPropertyStr := include_ucd_str "auxiliary/GraphemeBreakProperty.txt"
 
 /-- Includes the WordBreakProperty.txt string. -/
-def wordBreakPropertyStr := include_str "../UCD/auxiliary/WordBreakProperty.txt"
+def wordBreakPropertyStr := include_ucd_str "auxiliary/WordBreakProperty.txt"
   
 /-- Includes the SentenceBreakProperty.txt string. -/
-def sentenceBreakPropertyStr := include_str "../UCD/auxiliary/SentenceBreakProperty.txt"
+def sentenceBreakPropertyStr := include_ucd_str "auxiliary/SentenceBreakProperty.txt"
 
 /-!
   ### Emoji Subdirectory
 -/
 
 /-- Includes the emoji-data.txt string. -/
-def emojiDataStr := include_str "../UCD/emoji/emoji-data.txt"
+def emojiDataStr := include_ucd_str "emoji/emoji-data.txt"
 
 /-!
   ### Extracted Subdirectory
 -/
 
 /-- Includes the DerivedBidiClass.txt string. -/
-def derivedBidiClassStr := include_str "../UCD/extracted/DerivedBidiClass.txt"
+def derivedBidiClassStr := include_ucd_str "extracted/DerivedBidiClass.txt"
 
 /-- Includes the DerivedJoiningGroup.txt string. -/
-def derivedJoiningGroupStr := include_str "../UCD/extracted/DerivedJoiningGroup.txt"
+def derivedJoiningGroupStr := include_ucd_str "extracted/DerivedJoiningGroup.txt"
 
 /-- Includes the DerivedBinaryProperties.txt string. -/
-def derivedBinaryPropertiesStr := include_str "../UCD/extracted/DerivedBinaryProperties.txt"
+def derivedBinaryPropertiesStr := include_ucd_str "extracted/DerivedBinaryProperties.txt"
 
 /-- Includes the DerivedJoiningType.txt string. -/
-def derivedJoiningTypeStr := include_str "../UCD/extracted/DerivedJoiningType.txt"
+def derivedJoiningTypeStr := include_ucd_str "extracted/DerivedJoiningType.txt"
 
 /-- Includes the DerivedCombiningClass.txt string. -/
-def derivedCombiningClassStr := include_str "../UCD/extracted/DerivedCombiningClass.txt"
+def derivedCombiningClassStr := include_ucd_str "extracted/DerivedCombiningClass.txt"
 
 /-- Includes the DerivedLineBreak.txt string. -/
-def derivedLineBreakStr := include_str "../UCD/extracted/DerivedLineBreak.txt"
+def derivedLineBreakStr := include_ucd_str "extracted/DerivedLineBreak.txt"
 
 /-- Includes the DerivedDecompositionType.txt string. -/
-def derivedDecompositionTypeStr := include_str "../UCD/extracted/DerivedDecompositionType.txt"
+def derivedDecompositionTypeStr := include_ucd_str "extracted/DerivedDecompositionType.txt"
 
 /-- Includes the DerivedName.txt string. -/
-def derivedNameStr := include_str "../UCD/extracted/DerivedName.txt"
+def derivedNameStr := include_ucd_str "extracted/DerivedName.txt"
 
 /-- Includes the DerivedEastAsianWidth.txt string. -/
-def derivedEastAsianWidthStr := include_str "../UCD/extracted/DerivedEastAsianWidth.txt"
+def derivedEastAsianWidthStr := include_ucd_str "extracted/DerivedEastAsianWidth.txt"
 
 /-- Includes the DerivedNumericType.txt string. -/
-def derivedNumericTypeStr := include_str "../UCD/extracted/DerivedNumericType.txt"
+def derivedNumericTypeStr := include_ucd_str "extracted/DerivedNumericType.txt"
 
 /-- Includes the DerivedGeneralCategory.txt string. -/
-def derivedGeneralCategoryStr := include_str "../UCD/extracted/DerivedGeneralCategory.txt"
+def derivedGeneralCategoryStr := include_ucd_str "extracted/DerivedGeneralCategory.txt"
 
 /-- Includes the DerivedNumericValues.txt string. -/
-def derivedNumericValuesStr := include_str "../UCD/extracted/DerivedNumericValues.txt"
+def derivedNumericValuesStr := include_ucd_str "extracted/DerivedNumericValues.txt"
 
 /-!
   ### Unihan Subdirectory
 -/
 
 /-- Includes the Unihan_DictionaryIndices.txt string. -/
-def unihanDictionaryIndicesStr := include_str "../UCD/Unihan/Unihan_DictionaryIndices.txt"
+def unihanDictionaryIndicesStr := include_ucd_str "Unihan/Unihan_DictionaryIndices.txt"
 
 /-- Includes the Unihan_OtherMappings.txt string. -/
-def unihanOtherMappingsStr := include_str "../UCD/Unihan/Unihan_OtherMappings.txt"
+def unihanOtherMappingsStr := include_ucd_str "Unihan/Unihan_OtherMappings.txt"
 
 /-- Includes the Unihan_DictionaryLikeData.txt string. -/
-def unihanDictionaryLikeDataStr := include_str "../UCD/Unihan/Unihan_DictionaryLikeData.txt"
+def unihanDictionaryLikeDataStr := include_ucd_str "Unihan/Unihan_DictionaryLikeData.txt"
 
 /-- Includes the Unihan_RadicalStrokeCounts.txt string. -/
-def unihanRadicalStrokeCountsStr := include_str "../UCD/Unihan/Unihan_RadicalStrokeCounts.txt"
+def unihanRadicalStrokeCountsStr := include_ucd_str "Unihan/Unihan_RadicalStrokeCounts.txt"
 
 /-- Includes the Unihan_IRGSources.txt string. -/
-def unihanIRGSourcesStr := include_str "../UCD/Unihan/Unihan_IRGSources.txt"
+def unihanIRGSourcesStr := include_ucd_str "Unihan/Unihan_IRGSources.txt"
 
 /-- Includes the Unihan_Readings.txt string. -/
-def unihanReadingsStr := include_str "../UCD/Unihan/Unihan_Readings.txt"
+def unihanReadingsStr := include_ucd_str "Unihan/Unihan_Readings.txt"
 
 /-- Includes the Unihan_NumericValues.txt string. -/
-def unihanNumericValuesStr := include_str "../UCD/Unihan/Unihan_NumericValues.txt"
+def unihanNumericValuesStr := include_ucd_str "Unihan/Unihan_NumericValues.txt"
 
 /-- Includes the Unihan_Variants.txt string. -/
-def unihanVariantsStr := include_str "../UCD/Unihan/Unihan_Variants.txt"
+def unihanVariantsStr := include_ucd_str "Unihan/Unihan_Variants.txt"
 
 /-!
   ## HashMap Thunks
